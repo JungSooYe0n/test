a46d0bcd1492fcc08e12e67e60e6d105b4bfe515
likenan <15738392553@163.com>
2020-12-31 11:34:29 +0800

    预警标红关键词出现分组名称问题修复

--- netInsight-common/src/main/java/com/trs/netInsight/widget/alert/entity/AlertRule.java
+++ netInsight-common/src/main/java/com/trs/netInsight/widget/alert/entity/AlertRule.java
@@ -614,12 +614,12 @@ public class AlertRule extends BaseEntity {
 					database.add(Const.HYBASE_OVERSEAS);
 				}
 				if(this.statusTrsl != null && !"".equals(this.statusTrsl)){
-					String trsl_1  = FtsFieldConst.FIELD_GROUPNAME +  ":(\"微博\") AND (" + this.statusTrsl +")";
+					String trsl_1  = FtsFieldConst.FIELD_GROUPNAME +  ":(微博) AND (" + this.statusTrsl +")";
 					trsl = "".equals(trsl) ? "("+trsl_1 + ")" : trsl +" OR (" + trsl_1  + ")";
 					database.add(Const.WEIBO);
 				}
 				if(this.weChatTrsl != null && !"".equals(this.weChatTrsl)){
-					String trsl_1 = FtsFieldConst.FIELD_GROUPNAME +  ":(\"国内微信\" OR \"微信\" ) AND (" + this.weChatTrsl +")";
+					String trsl_1 = FtsFieldConst.FIELD_GROUPNAME +  ":(国内微信 OR 微信 ) AND (" + this.weChatTrsl +")";
 					trsl = "".equals(trsl) ? "("+trsl_1+ ")" : trsl +" OR (" + trsl_1 + ")";
 					database.add(Const.WECHAT_COMMON);
 				}
