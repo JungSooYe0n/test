7136baa1ebd59a620f8b5f8b58c3bd3e9e69cb60
likenan <15738392553@163.com>
2021-06-08 16:11:00 +0800

    屏蔽命中微博位置信息，希望可以屏蔽被转发博文中的位置信息

--- netInsight-alert/src/main/java/com/trs/netInsight/widget/special/service/impl/InfoListServiceImpl.java
+++ netInsight-alert/src/main/java/com/trs/netInsight/widget/special/service/impl/InfoListServiceImpl.java
@@ -6353,7 +6353,7 @@ public class InfoListServiceImpl implements IInfoListService {
 						//微博筛选  ----  微博筛选时 ，屏蔽微博原发 - 为转发、 屏蔽微博转发 - 为原发
 						if (sourceList.contains(Const.GROUPNAME_WEIBO) && (preciseFilterList.contains("notWeiboForward") || preciseFilterList.contains("notWeiboPrimary")
 						|| preciseFilterList.contains("notWeiboOrgAuthen") || preciseFilterList.contains("notWeiboPeopleAuthen")
-						|| preciseFilterList.contains("notWeiboAuthen") || preciseFilterList.contains("notWeiboLocation")
+						|| preciseFilterList.contains("notWeiboAuthen") || preciseFilterList.contains("notWeiboLocation")||preciseFilterList.contains("notWeiboRtLocation")
 						|| preciseFilterList.contains("notWeiboScreenName") || preciseFilterList.contains("notWeiboTopic")
 						)) {
 							if (buffer.length() > 0) {
@@ -6412,6 +6412,9 @@ public class InfoListServiceImpl implements IInfoListService {
 								}
 								if (preciseFilterList.contains("notWeiboLocation")) {//屏蔽命中微博位置信息
 									buffer.append(" NOT (").append(FtsFieldConst.FIELD_LOCATION).append(":(").append(childTrsl3.toString()).append(") OR ").append(FtsFieldConst.FIELD_LOCATION_LIKE).append(":(").append(childTrsl3.toString()).append("))");
+								}
+								if (preciseFilterList.contains("notWeiboRtLocation")) {//屏蔽被转发微博位置信息
+									buffer.append(" NOT (").append(FtsFieldConst.FIELD_RT_LOCATION).append(":(").append(childTrsl3.toString()).append(")) ");
 								}
 								if (preciseFilterList.contains("notWeiboScreenName")) {//忽略命中微博博主名
 									buffer.append(" NOT (").append(FtsFieldConst.FIELD_AUTHORS_LIKE).append(":(").append(childTrsl3.toString()).append("))");
