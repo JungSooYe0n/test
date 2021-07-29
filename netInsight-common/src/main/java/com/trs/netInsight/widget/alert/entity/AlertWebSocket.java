2565cb11cc3ef28dc532064ecc6e203175d413f0
gx150204 <798573821@qq.com>
2021-07-29 18:09:41 +0800

    预警弹窗-自动+创建时间排序字段更改

--- netInsight-common/src/main/java/com/trs/netInsight/widget/alert/entity/AlertWebSocket.java
+++ netInsight-common/src/main/java/com/trs/netInsight/widget/alert/entity/AlertWebSocket.java
@@ -1,8 +1,11 @@
 package com.trs.netInsight.widget.alert.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="alert_websocket")
@@ -11,29 +14,31 @@ import javax.persistence.*;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class AlertWebSocket{
 
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Id
-    private int id;
+    private int id;//自增项Id
 
-    @Column(name="receive_id")
+    @Column(name="receive_id")//接收用户
     private String receiveid;
 
-    @Column(name="receive_message")
+    @Column(name="receive_message")//接收数据
     private String receivemessage;
 
-    @Column(name="receive_time")
+    @Column(name="receive_time")//数据原时间-从查出数据中取出
     private String receivetime;
 
-    @Column(name="receive_from")
+    @Column(name="receive_from")//数据来源
     private String receivefrom;
 
-    @Column(name="receive_url")
+    @Column(name="receive_url")//数据原地址
     private String receiveurl;
 
-    @Column(name="timestamp")
-    private String timestamp;
+    @CreatedDate
+    @Column(name="create_date")//创建时间，用来排序
+    private Date createdate;
 
 
 }
--- netInsight-common/src/main/java/com/trs/netInsight/widget/alert/entity/repository/AlertWebSocketRepository.java
+++ netInsight-common/src/main/java/com/trs/netInsight/widget/alert/entity/repository/AlertWebSocketRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Repository("alertWebSocketRepository")
 public interface AlertWebSocketRepository extends JpaSpecificationExecutor<AlertWebSocket>, JpaRepository<AlertWebSocket, String> {
     //根据ID查询预警信息最新的前三条
-    @Query(value = "select * from alert_websocket where receive_id=:receiveid order by timestamp desc limit 3",nativeQuery = true)
+    @Query(value = "select * from alert_websocket where receive_id=:receiveid order by create_date desc limit 3",nativeQuery = true)
     public List<AlertWebSocket> selectAlertWebSocket(@Param("receiveid") String receiveid);
     //删除相关信息，保证每次存储后查出来的一定是最新的
     @Transactional
